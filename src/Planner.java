c0d8eb500870d252348377619be4d4132def275e
Jomot <Tomjones3198@gmail.com>
2023-04-06 18:06:12 +0100

    Added menu to CLI

--- src/Planner.java
+++ src/Planner.java
@@ -1,6 +1,37 @@
+import java.text.ParseException;
+import java.util.Scanner;
+
 public class Planner {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws ParseException {
+        Scanner scanner = new Scanner(System.in);
+        displayMenu();
+        switch (scanner.nextInt()) {
+            case 0 -> addMeal();
+            case 1 -> editMeal();
+            case 2 -> generatePlan();
+            default -> throw new ParseException("Invalid option entered", 0);
+        }
     }
 
+    private static void addMeal() {
+
+    }
+
+    private static void editMeal() {
+
+    }
+
+    private static void generatePlan() {
+
+    }
+
+    private static void displayMenu() {
+        System.out.println("--------Menu--------");
+        System.out.println("-- 1 - Add Meal ----");
+        System.out.println("-- 2 - Edit Meal ---");
+        System.out.println("-- 3 - Plan Meals --");
+        System.out.println("--------------------");
+        System.out.println("Enter:");
+
+    }
 }
